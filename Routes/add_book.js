280c1d70b5d72a80d9c91a37bd1f816ca74a0f26
Billy <billyjcatchpole@gmail.com>
2020-04-18 14:09:35 +0100

    43423

--- Routes/add_book.js
+++ Routes/add_book.js
@@ -1,9 +1,6 @@
-const path = require("path");
-
 //Controllers
-//server.use(express.static(path.join(__dirname, 'public')));//Allow the html to connect to css pages
-const add_book_controller = require(path.join(__dirname, "controllers/add_book_controller.js"))
-  //  "../controllers/add_book_controller");
+const add_book_controller = require("../controllers/add_book_controller");
+const Book = require("../Models/Book")
 
 //External
 const express = require("express");
@@ -12,7 +9,25 @@ const express = require("express");
 const router = express.Router();
 
 //route
-router.post("/add_book", add_book_controller.add_book);
+router.post("/add_book", async (req, res, next) => {
+
+    const year = req.body.form_values.year;//grab the year 
+    const condition = req.body.form_values.condition//and condition from the request
+
+    const book_exists = await Book.findOne({ year: year, condition: condition })//check if the given year and condition already exists in the database
+
+    if (book_exists) return res.status(409).json({ error: "Book already exists with that year and condition. Only 1 copy of each condition is allowed for each year.", book:req.body.form_values})
+
+    const new_book = new Book({year:year, condition:condition} )//create a new book with the given input
+
+    const book_saved = await new_book.save()//save the new book
+
+    //if it saved successfully, send a 201 status, if it failed, send a database error
+    book_saved ? res.status(201).json({message: "Book saved successfully", success:true, book:req.body.form_values}) : res.status(500).json({error:"Something went wrong with the database"})
+
+});
 
 
 module.exports = router;
+
+
